366d1674bf4f8860e7364c2567e6ae0832568ef2
Alexey Voronin <opener@Alexeys-Mac.local>
2015-11-19 07:11:15 -0800

    Detail view templated

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -53,6 +53,10 @@ describe('PhoneCat App', function() {
     it('should display placeholder page with phoneId', function() {
       expect(element(by.binding('phoneId')).getText()).toBe('nexus-s');
     });
+
+    it('should display nexus-s page', function() {
+      expect(element(by.binding('phone.name')).getText()).toBe('Nexus S');
+    });
   });
 
 });
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -34,4 +34,25 @@ describe('PhoneCat controllers', function() {
 	  expect(scope.orderProp).toBe('age');
 	});
   });
+
+  describe('PhoneDetailCtrl', function(){
+    var scope, $httpBackend, ctrl;
+
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $routeParams, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('phones/xyz.json').respond({name:'phone xyz'});
+
+      $routeParams.phoneId = 'xyz';
+      scope = $rootScope.$new();
+      ctrl = $controller('PhoneDetailCtrl', {$scope: scope});
+    }));
+
+
+    it('should fetch phone detail', function() {
+      expect(scope.phone).toBeUndefined();
+      $httpBackend.flush();
+
+      expect(scope.phone).toEqual({name:'phone xyz'});
+    });
+  });
 });
