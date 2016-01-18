6f05993453b5ca9a4f01a1f07e011fe40f3e376b
Sky Flyer <skyflyer@Skys-Mac.local>
2016-01-18 04:12:18 -0800

    minor change

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,7 +6,7 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
   function ($scope, $http) {
     $http.get('phones/phones.json').success(function(data) {
       $scope.phones = data;
-    });
+    }); 
 
     $scope.orderProp = 'age';
   }]);
